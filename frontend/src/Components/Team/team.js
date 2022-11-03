936b9f6c6f26f1d28db842cd7bc629d9aa37dd5b
prince verma <prince.verma@oneassist.in>
2022-11-03 17:46:33 +0530

    add css file and handle routes

--- frontend/src/Components/Team/team.js
+++ frontend/src/Components/Team/team.js
@@ -1,35 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import volPrince from './Images/volPrince.png';
 import { Row, Col } from 'react-bootstrap';
 import './team.css'
 function Team() {
-    function showDetails(show){
-        var det = document.getElementById("det-1");
-        var vimg = document.getElementById("img-1");
-        if(show){
-            det.style.setProperty('display', 'inline');
-            vimg.style.setProperty('display', 'none');
-        }
-        else{
-            det.style.setProperty('display', 'none');
-            vimg.style.setProperty('display', 'inline'); 
-        }
-    }
+
+    const [hideMemberDetails,setHideMemberDetails] = useState(false);
+    const [hideMemberImage,setHideMemberImage] = useState(true);
+
+
+    const handleMemberImg = () => {
+        setHideMemberDetails(()=>!hideMemberDetails);
+        setHideMemberImage(()=>!hideMemberImage);
+    };
+
+    const handleMemberDetails = () =>{
+        setHideMemberDetails(()=>!hideMemberDetails);
+        setHideMemberImage(()=>!hideMemberImage);
+    };
     return (
         <>
         <Row>
             <Col md={5} className="tm-vol-img">
-                <img src={volPrince} alt="Prince Verma" 
-                onMouseDown={() => showDetails(true)}
-                onMouseUp={() => showDetails(false)}
-                id="img-1"/>
+                {hideMemberImage && <img src={volPrince} alt="Prince Verma"
+                     onMouseOver={handleMemberImg}
+                     id="img-1"/>}
                 <div className="tm-vol-name">
                     Prince Verma | Founder
                 </div>
-                <div className="tm-vol-det" id="det-1">
+                {hideMemberDetails && <div className="tm-vol-det" id="det-1"  onMouseOut={handleMemberDetails}>
                     Prince Verma<br />
                     Open Source Contributer, Author of 3 Books, Software Engineer at OneAssist Consumer Solutions
-                </div>
+                </div>}
             </Col>
             </Row>
         </>
