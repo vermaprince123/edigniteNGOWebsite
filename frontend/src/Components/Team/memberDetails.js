4400790265b2c38c4d303b0e31379bfb30859a6c
prince verma <prince.verma@oneassist.in>
2022-11-03 20:06:13 +0530

    add css file and handle routes

---
+++ frontend/src/Components/Team/memberDetails.js
@@ -0,0 +1,24 @@
+import princeImage from './Images/volPrince.png';
+import dummyImage from './Images/dummyImage.png';
+const membersDetails = [
+    {
+        "id":1,
+        "name":"Prince Verma",
+        "srcImage":princeImage,
+        "alt":"prince verma",
+        "designation":"Founder",
+        "occupation":"Software Engineer, OneAssist Consumer Solutions",
+        "achivements":"Open Source Contributer, Author of three books"
+    },
+    {
+        "id":2,
+        "name":"edignite",
+        "srcImage":dummyImage,
+        "alt":"edignite image",
+        "designation":"dummy data edignite",
+        "occupation":"dummy data how are u",
+        "achivements":"dummy data i am fine"
+    }
+];
+
+export  default  membersDetails;
--- frontend/src/Components/Team/team.js
+++ frontend/src/Components/Team/team.js
@@ -1,38 +1,59 @@
 import React, {useState} from "react";
-import volPrince from './Images/volPrince.png';
 import { Row, Col } from 'react-bootstrap';
-import './team.css'
+import './team.css';
+import membersDetails from './memberDetails';
+import memberDetails from "./memberDetails";
+
+
 function Team() {
+    const [membersDetailsCloneData, setmembersDetailsCloneData] = useState(membersDetails);
+    const [memberId, setmemberId] = useState(null);
 
-    const [hideMemberDetails,setHideMemberDetails] = useState(false);
-    const [hideMemberImage,setHideMemberImage] = useState(true);
 
-
-    const handleMemberImg = () => {
-        setHideMemberDetails(()=>!hideMemberDetails);
-        setHideMemberImage(()=>!hideMemberImage);
+    const handleMemberImg = (id) => {
+        setmemberId(id);
+        const selectedMember = membersDetails.filter((member)=>member.id == id);
+        setmembersDetailsCloneData(selectedMember);
     };
 
-    const handleMemberDetails = () =>{
-        setHideMemberDetails(()=>!hideMemberDetails);
-        setHideMemberImage(()=>!hideMemberImage);
+    const handleMemberDetails = (id) =>{
+        setmemberId("");
+        setmembersDetailsCloneData(memberDetails);
     };
     return (
         <>
         <Row>
             <Col md={5} className="tm-vol-img">
-                {/* {hideMemberImage && <img src={volPrince} alt="Prince Verma"
-                     onMouseOver={handleMemberImg}
-                     id="img-1"/>}
-                {hideMemberDetails && <div className="tm-vol-det" id="det-1"  onMouseOut={handleMemberDetails}>
-                    Prince Verma<br />
-                    Open Source Contributer, Author of 3 Books, Software Engineer at OneAssist Consumer Solutions
-                </div>} */}
-                <div className="tm-vol-det" id="det-1"  onMouseOut={handleMemberDetails}>
-                    <div className="tm-vol-nd">Prince Verma | Founder</div>
-                    <div className="tm-vol-occ">Software Engineer, OneAssist Consumer Solutions</div>
-                    <div className="tm-vol-ach">Open Source Contributer, Author of three books</div>
-                </div>
+
+                { membersDetailsCloneData.map((memberDetail)=>{
+
+                        if(memberDetail.id == memberId){
+                            const otherMember = membersDetails.filter((member)=>member.id != memberId);
+                            return(
+                                <div key={memberDetail.id}>
+                                    <div  className="tm-vol-det" id="det-1"  onMouseOut={()=>handleMemberDetails(memberDetail.id)}>
+                                        <div className="tm-vol-nd"> { memberDetail.name | memberDetail.designation } </div>
+                                        <div className="tm-vol-occ">{ memberDetail.occupation }</div>
+                                        <div className="tm-vol-ach">{ memberDetail.achivements } </div>
+                                    </div>
+                                    <img src={otherMember[0].srcImage} alt={otherMember[0].alt}
+                                         onMouseOver={()=>handleMemberImg(otherMember[0].id)}
+                                         id="img-1"/>
+                                </div>
+                            )
+                        }else{
+                            return (
+                                <div key={memberDetail.id}>
+                                    <img src={memberDetail.srcImage} alt={memberDetail.alt}
+                                         onMouseOver={()=>handleMemberImg(memberDetail.id)}
+                                         id="img-1"/>
+                                </div>
+                            )
+                        }
+
+
+                  })
+                }
             </Col>
             </Row>
         </>
