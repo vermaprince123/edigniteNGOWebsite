0b763b56a2180aad6dba615e07f2f2c24e9dcd75
HeerMehta <mehtaheerh@gmail.com>
2022-11-03 17:13:44 +0530

    mouseev

--- frontend/src/Components/Team/team.js
+++ frontend/src/Components/Team/team.js
@@ -1,14 +1,37 @@
 import React from "react";
-import volPrince from './Images/volPrince.png'
-
-
-function Team(){
-    return(
+import volPrince from './Images/volPrince.png';
+import { Row, Col } from 'react-bootstrap';
+import './team.css'
+function Team() {
+    function showDetails(show){
+        var det = document.getElementById("det-1");
+        var vimg = document.getElementById("img-1");
+        if(show){
+            det.style.setProperty('display', 'inline');
+            vimg.style.setProperty('display', 'none');
+        }
+        else{
+            det.style.setProperty('display', 'none');
+            vimg.style.setProperty('display', 'inline'); 
+        }
+    }
+    return (
         <>
-        <div className="tm-vol-img">
-        <img src={volPrince} alt="Prince Verma"/>
-        <div></div>
-        </div>
+        <Row>
+            <Col md={5} className="tm-vol-img">
+                <img src={volPrince} alt="Prince Verma" 
+                onMouseDown={() => showDetails(true)}
+                onMouseUp={() => showDetails(false)}
+                id="img-1"/>
+                <div className="tm-vol-name">
+                    Prince Verma | Founder
+                </div>
+                <div className="tm-vol-det" id="det-1">
+                    Prince Verma<br />
+                    Open Source Contributer, Author of 3 Books, Software Engineer at OneAssist Consumer Solutions
+                </div>
+            </Col>
+            </Row>
         </>
     )
 }
