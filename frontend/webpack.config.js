a5c4fefb1fe96e906f135868b99fe0e5713133e0
HeerMehta <mehtaheerh@gmail.com>
2022-11-10 15:53:28 +0530

    compression

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,31 +3,31 @@ const hash = new Date().getTime();
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports={
-  mode:process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry:{
-    main:path.resolve(__dirname, 'src/index.js')
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js')
   },
-  output:{
-    path:path.resolve(__dirname, 'build'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
     filename: `static/js/[name].${hash}.js`,
-    clean:true
+    clean: true
   },
-  devtool:'inline-source-map',
-  devServer:{
-    port:5000,
-    open:true,
-    hot:true
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 5000,
+    open: true,
+    hot: true
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-        test:/\.css$/, use:['style-loader', 'css-loader']
+        test: /\.css$/, use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
         type: 'asset/inline',
-    },
+      },
       {
         test: /\.(svg|png|ico|webp|jpg|gif|jpeg|JPG|jfif|woff|woff2)$/,
         loader: 'file-loader',
@@ -37,13 +37,13 @@ module.exports={
       },
       {
         test: /\.js$/,
-        exclude:/node_modules/,
-        use:{
-          loader:'babel-loader',
-          options:{
-            presets:[
-                '@babel/preset-env',
-                '@babel/preset-react'
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
             ]
           }
         }
@@ -60,11 +60,11 @@ module.exports={
     ],
   },
 
-  plugins:[
-      new HtmlWebpackPlugin({
-        title:'Edignite NGO',
-        filename:'index.html',
-        template:path.resolve(__dirname, 'public/index.html')
-      })
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Edignite NGO',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'public/index.html')
+    })
   ]
 };
