21a98eea6909b2cb7040ee4684c1192c7694e2e7
HeerMehta <mehtaheerh@gmail.com>
2022-11-11 14:32:49 +0530

    teamPhoto

--- frontend/src/Components/Team/team.js
+++ frontend/src/Components/Team/team.js
@@ -18,20 +18,20 @@ function Team() {
                 <Row>
                     {membersDetailsCloneData.map((memberDetail) => {
                         return (
-                            <Col md={4} key={memberDetail.id} className="tm-vol-mem my-2">
+                            <Col md={3} key={memberDetail.id} className="tm-vol-mem my-2">
                                 <img src={memberDetail.srcImage} alt={memberDetail.alt}
                                     id="img-1" />
                                 <div className="tm-vol-det text-center mt-3">
                                     <span className="tm-vol-nd"> {memberDetail.name}, {memberDetail.designation} </span><br />
                                     <span className="tm-vol-occ">{memberDetail.occupation}</span>
                                     <Row className="tm-vol-sm">
-                                        <Col className="tm-vol-sm-lnk" xs={4}>
+                                        <Col className="tm-vol-sm-lnk text-center" xs={4}>
                                             <a className="tm-sm-lnk" href={memberDetail.instaId}><Instagram size={20} /></a>
                                         </Col>
-                                        <Col className="tm-vol-sm-lnk" xs={4}>
+                                        <Col className="tm-vol-sm-lnk text-center" xs={4}>
                                             <a className="tm-sm-lnk" href={memberDetail.fbId}><Facebook size={20} /></a>
                                         </Col>
-                                        <Col className="tm-vol-sm-lnk" xs={4}>
+                                        <Col className="tm-vol-sm-lnk text-center" xs={4}>
                                             <a className="tm-sm-lnk" href={memberDetail.lnkdinId}><Linkedin size={20} /></a>
                                         </Col>
                                     </Row>
