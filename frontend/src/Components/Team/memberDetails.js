258d35d5c9b094e97e1eea9a3d0610ccb6a572af
HeerMehta <mehtaheerh@gmail.com>
2022-12-01 14:32:26 +0530

    team update

--- frontend/src/Components/Team/memberDetails.js
+++ frontend/src/Components/Team/memberDetails.js
@@ -1,10 +1,24 @@
-import princeImage from './Images/volPrince.png';
-import dummyImage from './Images/dummyImage.png';
+import imgPrince from './Images/volPrince.png';
+import imgVishal from './Images/volVishal.png';
+import imgHeer from './Images/volHeer.png';
+import imgSarita from './Images/volSarita.png';
+import imgJyoti from './Images/volJyoti.png';
+import imgAnusree from './Images/volAnusree.png';
+import imgPavan from './Images/volPavan.png';
+import imgBrijesh from './Images/volBrijesh.png';
+import imgFiroj from './Images/volFiroj.png';
+import imgNeela from './Images/volNeela.png';
+import imgAlankrita from './Images/volAlankrita.png';
+import imgTejal from './Images/volTejal.png';
+import imgArvind from './Images/volArvind.png';
+import imgAmarendra from './Images/volAmarendra.png';
+import imgSuswara from './Images/volSuswara.png';
+import imgShruti from './Images/volShruti.png';
 const membersDetails = [
     {
         "id":1,
         "name":"Prince Verma",
-        "srcImage":princeImage,
+        "srcImage":imgPrince,
         "alt":"prince verma",
         "designation":"Founder",
         "occupation":"Software Engineer, OneAssist Consumer Solutions",
@@ -16,7 +30,7 @@ const membersDetails = [
     {
         "id":2,
         "name":"Vishal Sachan",
-        "srcImage":princeImage,
+        "srcImage":imgVishal,
         "alt":"prince verma",
         "designation":"Co-Founder",
         "occupation":"Final Year M.Sc. Chemistry, NIT Surat",
@@ -28,7 +42,7 @@ const membersDetails = [
     {
         "id": 3,
         "name":"Heer Mehta",
-        "srcImage":princeImage,
+        "srcImage":imgHeer,
         "alt":"prince verma",
         "designation":"Minister",
         "occupation":"Final Year B.Tech. Chemical Engg., NIT Surat",
@@ -40,7 +54,7 @@ const membersDetails = [
     {
         "id": 4,
         "name":"Alankrita Verma",
-        "srcImage":princeImage,
+        "srcImage":imgAlankrita,
         "alt":"prince verma",
         "designation":"Treasurer",
         "occupation":"Final Year, B.Tech. Electronics Engg., NIT Surat",
@@ -52,7 +66,7 @@ const membersDetails = [
     {
         "id": 5,
         "name":"Sarita Yadav",
-        "srcImage":princeImage,
+        "srcImage":imgSarita,
         "alt":"prince verma",
         "designation":"Head of Adisory",
         "occupation":"Final Year, B.Tech. Electronics Engg., NIT Surat",
@@ -64,7 +78,7 @@ const membersDetails = [
     {
         "id": 6,
         "name":"Dr. Jyoti Yadav",
-        "srcImage":princeImage,
+        "srcImage":imgJyoti,
         "alt":"prince verma",
         "designation":"Advisor",
         "occupation":"Physiotherapist",
@@ -76,7 +90,7 @@ const membersDetails = [
     {
         "id": 7,
         "name":"Pavan Gautam",
-        "srcImage":princeImage,
+        "srcImage":imgPavan,
         "alt":"prince verma",
         "designation":"Trustee and Teaching Head",
         "occupation":"4th Year M'Sc. Mathematics, NIT Surat",
@@ -88,7 +102,7 @@ const membersDetails = [
     {
         "id": 10,
         "name":"Anusree",
-        "srcImage":princeImage,
+        "srcImage":imgAnusree,
         "alt":"prince verma",
         "designation":"Member",
         "occupation":"B.Tech. Final Year Electrical Engg., NIT Surat",
@@ -100,7 +114,7 @@ const membersDetails = [
     {
         "id": 8,
         "name":"Brijesh Saini",
-        "srcImage":princeImage,
+        "srcImage":imgBrijesh,
         "alt":"prince verma",
         "designation":"Trustee and Social Media Management Head",
         "occupation":"BTech Final Year Chemical Engg., NIT Surat",
@@ -112,7 +126,7 @@ const membersDetails = [
     {
         "id": 9,
         "name":"Firoj Banjare",
-        "srcImage":princeImage,
+        "srcImage":imgFiroj,
         "alt":"prince verma",
         "designation":"Trustee and Distributions Head",
         "occupation":"4th Year M.Sc. Physics, NIT Surat",
@@ -124,7 +138,7 @@ const membersDetails = [
     {
         "id": 11,
         "name":"Neela Vasava",
-        "srcImage":princeImage,
+        "srcImage":imgNeela,
         "alt":"prince verma",
         "designation":"Trustee and Advisor",
         "occupation":"B.Tech. Final Year Electrical Engg., NIT Surat",
@@ -136,7 +150,7 @@ const membersDetails = [
     {
         "id": 12,
         "name":"Arvind Kushwaha",
-        "srcImage":princeImage,
+        "srcImage":imgArvind,
         "alt":"prince verma",
         "designation":"Trustee",
         "occupation":"4th Year M.Sc. Mathematics, NIT Surat",
@@ -148,7 +162,7 @@ const membersDetails = [
     {
         "id": 13,
         "name":"Nitish Chandaka",
-        "srcImage":princeImage,
+        "srcImage":imgNitish,
         "alt":"prince verma",
         "designation":"HR",
         "occupation":"B.Tech. Final Year, Chemical Engg., NIT Surat",
@@ -160,10 +174,34 @@ const membersDetails = [
     {
         "id": 14,
         "name":"V Sree Suswara",
-        "srcImage":princeImage,
+        "srcImage":imgSuswara,
         "alt":"prince verma",
         "designation":"Member",
         "occupation":"4th Year M.Sc. Physics, NIT Surat",
+        "achivements":"Open Source Contributer, Author of three books",
+        "instaId": "https://www.instagram.com/cypher_0010/",
+        "fbId": "https://www.facebook.com/firoj.banjare.3",
+        "lnkdinId": "https://www.linkedin.com/in/firoj-banjare-539b2319b/"
+    },
+    {
+        "id": 15,
+        "name":"Amarendra Singh",
+        "srcImage":imgAmarendra,
+        "alt":"prince verma",
+        "designation":"Trustee",
+        "occupation":"4th Year M.Sc. Chemistry, NIT Surat",
+        "achivements":"Open Source Contributer, Author of three books",
+        "instaId": "https://www.instagram.com/cypher_0010/",
+        "fbId": "https://www.facebook.com/firoj.banjare.3",
+        "lnkdinId": "https://www.linkedin.com/in/firoj-banjare-539b2319b/"
+    },
+    {
+        "id": 16,
+        "name":"Shruti Shah",
+        "srcImage":imgShruti,
+        "alt":"prince verma",
+        "designation":"Member",
+        "occupation":"3rd Year M.Sc. Mathematics, NIT Surat",
         "achivements":"Open Source Contributer, Author of three books",
         "instaId": "https://www.instagram.com/cypher_0010/",
         "fbId": "https://www.facebook.com/firoj.banjare.3",
