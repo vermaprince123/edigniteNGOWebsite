d19a4972c447a890b953e731033b75e0eafd7959
HeerMehta <mehtaheerh@gmail.com>
2022-12-08 16:58:35 +0530

    app changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import {
   Routes,
   Route,
-  Link
+  Link, Navigate
 } from 'react-router-dom';
 import logo from './Images/ediLogo.png'
 import Homepage from './Components/Homepage/homepage.js';
@@ -36,7 +36,7 @@ function App() {
               height="30"
               className="d-inline-block align-top"
             />
-            {   } Edignite NGO
+            { } Edignite NGO
           </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -52,14 +52,15 @@ function App() {
       </Navbar>
 
       <div className="app-sm bg-warning">
-      <a href="mailto:edignitengo@gmail.com?subject=Message to Edignite Team"  target="_blank"className="cnt-sm-lnk"><Envelope size={15} /></a><br />
-        <a href="https://www.instagram.com/ed_ignite/" target="_blank"><Instagram size={15}/></a><br />
-        <a href="https://www.facebook.com/socialindiagroup.in" target="_blank"><Facebook size={15}/></a><br />
-        <a href="https://www.linkedin.com/company/edignite-ngo/" target="_blank"><Linkedin size={15}/></a><br />
-        <a href="https://www.youtube.com/channel/UCJWu4OJBlFmFPF4sQpD6JIg" target="_blank"><Youtube size={15}/></a>
+        <a href="mailto:edignitengo@gmail.com?subject=Message to Edignite Team" target="_blank" className="cnt-sm-lnk"><Envelope size={15} /></a><br />
+        <a href="https://www.instagram.com/ed_ignite/" target="_blank"><Instagram size={15} /></a><br />
+        <a href="https://www.facebook.com/socialindiagroup.in" target="_blank"><Facebook size={15} /></a><br />
+        <a href="https://www.linkedin.com/company/edignite-ngo/" target="_blank"><Linkedin size={15} /></a><br />
+        <a href="https://www.youtube.com/channel/UCJWu4OJBlFmFPF4sQpD6JIg" target="_blank"><Youtube size={15} /></a>
       </div>
 
       <Routes>
+        <Route exact path="/:" element={<Homepage />} errorElement={<Homepage />}/>
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/places" element={<Places />} />
         <Route exact path="/team" element={<Team />} />
