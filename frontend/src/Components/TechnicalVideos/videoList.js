16520366ebf1583cfba9587099ffa3b6721d3052
HeerMehta <mehtaheerh@gmail.com>
2022-12-31 11:56:19 +0530

    video list changes

--- frontend/src/Components/TechnicalVideos/videoList.js
+++ frontend/src/Components/TechnicalVideos/videoList.js
@@ -6,18 +6,38 @@ const videoList = [
     },
     {
         "id": 2,
-        "title": "JavaScript Crash Course",
-        "src": "https://www.youtube.com/embed/WXnVFwu_Ty8"
+        "title": "Angular Crash Course",
+        "src": "https://www.youtube.com/embed/WmSYIBSKYSI"
     },
     {
         "id": 3,
-        "title": "JavaScript Crash Course",
-        "src": "https://www.youtube.com/embed/WXnVFwu_Ty8"
+        "title": "Visual Sorting Project with JavaScript",
+        "src": "https://www.youtube.com/embed/DknsMWaOMWs"
     },
     {
         "id": 4,
-        "title": "JavaScript Crash Course",
-        "src": "https://www.youtube.com/embed/WXnVFwu_Ty8"
+        "title": "Advanced JavaScript",
+        "src": "https://www.youtube.com/embed/dlNrL36mCEw"
+    },
+    {
+        "id": 5,
+        "title": "Advanced JavaScript",
+        "src": "https://www.youtube.com/embed/G3jy_RMEAz0"
+    },
+    {
+        "id": 6,
+        "title": "DSA Questions on String",
+        "src": "https://www.youtube.com/embed/dCpJ93Yn2Pk"
+    },
+    {
+        "id": 7,
+        "title": "DSA Problems in Arrays",
+        "src": "https://www.youtube.com/embed/RmFe8p7wxNY"
+    },
+    {
+        "id": 8,
+        "title": "Concept of Hashing in DSA",
+        "src": "https://www.youtube.com/embed/ByKBE5gokKQ"
     },
 ]
 
